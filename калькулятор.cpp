72be0a70bea2fc0b6ddd5a7eeb1c023651186657
Syrtsevnikita <145428432+SyrNik24@users.noreply.github.com>
2023-10-29 22:21:54 +0300

    Add files via upload

---
+++ калькулятор.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+    
+    //ввод числа пользователем
+    double num1, num2, res;
+    char a;
+    cout << "Введите первое число" << endl;
+    cin >> num1;
+    cout << "Введите второе число" << endl;
+    cin >> num2;
+    cout << "Введите действие(+,-,/,*)" << endl;
+    cin >> a;
+
+    //вычисление
+    if (a == '+') {            //"+"
+        res = num1 + num2;
+        cout << "Ответ:" << res;
+    }
+    else if(a=='-'){           //"-"
+        res = num1 - num2;
+        cout << "Ответ:" << res;
+    }
+    else if (a == '/') {       //"/"
+        res = num1 / num2;
+        cout << "Ответ:" << res;
+    }
+    else if (a == '*') {       //"*"
+        res = num1 * num2;
+        cout << "Ответ:" << res;
+    }
+    else
+        cout << "Вы ввели некорректные данные";
+
+    return 0;
+}
